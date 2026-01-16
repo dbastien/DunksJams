70ee3cc4d7f35e28b0226e2b844b351ea49609a2
dbastien <dwbastien@gmail.com>
2026-01-15 17:58:51 -0800

    Reorganize TweenExtensions.cs for better code organization
    - Group methods by functionality: Transform, UI, From, Relative, Generic
    - Move interpolation helpers to bottom of file
    - Improved readability and maintainability
    - Logical grouping of similar methods together
    - Better code organization following clean code principles

--- Assets/Scripts/Tween/TweenExtensions.cs
+++ Assets/Scripts/Tween/TweenExtensions.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public static class TweenExtensions
 {
+    // ===== TRANSFORM METHODS =====
+
     public static Tween<Vector3> MoveTo(this Transform t, Vector3 target, float d, EaseType e) =>
         AddTween(new Tween<Vector3>(t.position, target, d, null, v => t.position = v, Vector3.Lerp).SetEase(e));
 
@@ -12,13 +14,32 @@ public static class TweenExtensions
     public static Tween<Quaternion> RotateTo(this Transform t, Quaternion target, float d, EaseType e) =>
         AddTween(new Tween<Quaternion>(t.rotation, target, d, null, v => t.rotation = v, Quaternion.Lerp).SetEase(e));
 
+    public static Tween<Vector3> ScaleTo(this Transform t, Vector3 target, float d, EaseType e) =>
+        AddTween(new Tween<Vector3>(t.localScale, target, d, null, v => t.localScale = v, Vector3.Lerp).SetEase(e));
+
+    // ===== UI METHODS =====
+
     public static Tween<float> FadeTo(this CanvasGroup cg, float target, float d, EaseType e) =>
         AddTween(new Tween<float>(cg.alpha, target, d, null, v => cg.alpha = v, Mathf.Lerp).SetEase(e));
 
     public static Tween<Color> ColorTo(this SpriteRenderer sr, Color target, float d, EaseType e) =>
         AddTween(new Tween<Color>(sr.color, target, d, null, v => sr.color = v, Color.Lerp).SetEase(e));
 
-    // From tweening methods (tween from start value to current value)
+    // RectTransform methods (UI-specific)
+    public static Tween<Vector2> TweenAnchoredPosition(this RectTransform rt, Vector2 target, float d, EaseType e) =>
+        AddTween(new Tween<Vector2>(rt.anchoredPosition, target, d, null, v => rt.anchoredPosition = v, LerpVector2).SetEase(e));
+
+    public static Tween<Vector2> TweenAnchoredPosition(this RectTransform rt, Vector2 target, float d, Func<float, float> e) =>
+        AddTween(new Tween<Vector2>(rt.anchoredPosition, target, d, e, v => rt.anchoredPosition = v, LerpVector2));
+
+    public static Tween<Vector2> TweenSizeDelta(this RectTransform rt, Vector2 target, float d, EaseType e) =>
+        AddTween(new Tween<Vector2>(rt.sizeDelta, target, d, null, v => rt.sizeDelta = v, LerpVector2).SetEase(e));
+
+    public static Tween<Vector2> TweenSizeDelta(this RectTransform rt, Vector2 target, float d, Func<float, float> e) =>
+        AddTween(new Tween<Vector2>(rt.sizeDelta, target, d, e, v => rt.sizeDelta = v, LerpVector2));
+
+    // ===== FROM METHODS =====
+
     public static Tween<Vector3> MoveFrom(this Transform t, Vector3 startPos, float d, EaseType e) =>
         AddTween(new Tween<Vector3>(startPos, t.position, d, null, v => t.position = v, Vector3.Lerp).SetEase(e));
 
@@ -37,25 +58,37 @@ public static class TweenExtensions
     public static Tween<Color> ColorFrom(this SpriteRenderer sr, Color startColor, float d, EaseType e) =>
         AddTween(new Tween<Color>(startColor, sr.color, d, null, v => sr.color = v, Color.Lerp).SetEase(e));
 
-    // Relative tweening methods
+    // ===== RELATIVE METHODS =====
+
     public static Tween<Vector3> MoveBy(this Transform t, Vector3 offset, float d, EaseType e) =>
         AddTween(new Tween<Vector3>(t.position, t.position + offset, d, null, v => t.position = v, Vector3.Lerp).SetEase(e));
 
     public static Tween<Vector3> MoveBy(this Transform t, Vector3 offset, float d, Func<float, float> e) =>
         AddTween(new Tween<Vector3>(t.position, t.position + offset, d, e, v => t.position = v, Vector3.Lerp));
 
-    public static Tween<Vector3> ScaleTo(this Transform t, Vector3 target, float d, EaseType e) =>
-        AddTween(new Tween<Vector3>(t.localScale, target, d, null, v => t.localScale = v, Vector3.Lerp).SetEase(e));
-
     public static Tween<Vector3> ScaleBy(this Transform t, Vector3 scaleOffset, float d, EaseType e) =>
         AddTween(new Tween<Vector3>(t.localScale, t.localScale + scaleOffset, d, null, v => t.localScale = v, Vector3.Lerp).SetEase(e));
 
-    // Support for missing types: Vector2, int, Rect
+    // ===== GENERIC STATIC METHODS =====
 
-    // Vector2 interpolation helper
+    // Int tweening - generic for any int property
+    public static Tween<int> TweenInt(int startValue, int endValue, float duration, Action<int> setter, EaseType easeType) =>
+        AddTween(new Tween<int>(startValue, endValue, duration, null, setter, LerpInt).SetEase(easeType));
+
+    public static Tween<int> TweenInt(int startValue, int endValue, float duration, Action<int> setter, Func<float, float> customEase) =>
+        AddTween(new Tween<int>(startValue, endValue, duration, customEase, setter, LerpInt));
+
+    // Rect tweening - generic for any Rect property
+    public static Tween<Rect> TweenRect(Rect startValue, Rect endValue, float duration, Action<Rect> setter, EaseType easeType) =>
+        AddTween(new Tween<Rect>(startValue, endValue, duration, null, setter, LerpRect).SetEase(easeType));
+
+    public static Tween<Rect> TweenRect(Rect startValue, Rect endValue, float duration, Action<Rect> setter, Func<float, float> customEase) =>
+        AddTween(new Tween<Rect>(startValue, endValue, duration, customEase, setter, LerpRect));
+
+    // ===== INTERPOLATION HELPERS =====
+
     static Vector2 LerpVector2(Vector2 a, Vector2 b, float t) => Vector2.Lerp(a, b, t);
 
-    // Rect interpolation helper (interpolate each component)
     static Rect LerpRect(Rect a, Rect b, float t) => new Rect(
         Mathf.Lerp(a.x, b.x, t),
         Mathf.Lerp(a.y, b.y, t),
@@ -63,35 +96,7 @@ public static class TweenExtensions
         Mathf.Lerp(a.height, b.height, t)
     );
 
-    // Int interpolation helper (lerp and round)
     static int LerpInt(int a, int b, float t) => Mathf.RoundToInt(Mathf.Lerp(a, b, t));
-
-    // Vector2 tweening extensions
-    public static Tween<Vector2> TweenAnchoredPosition(this RectTransform rt, Vector2 target, float d, EaseType e) =>
-        AddTween(new Tween<Vector2>(rt.anchoredPosition, target, d, null, v => rt.anchoredPosition = v, LerpVector2).SetEase(e));
-
-    public static Tween<Vector2> TweenAnchoredPosition(this RectTransform rt, Vector2 target, float d, Func<float, float> e) =>
-        AddTween(new Tween<Vector2>(rt.anchoredPosition, target, d, e, v => rt.anchoredPosition = v, LerpVector2));
-
-    public static Tween<Vector2> TweenSizeDelta(this RectTransform rt, Vector2 target, float d, EaseType e) =>
-        AddTween(new Tween<Vector2>(rt.sizeDelta, target, d, null, v => rt.sizeDelta = v, LerpVector2).SetEase(e));
-
-    public static Tween<Vector2> TweenSizeDelta(this RectTransform rt, Vector2 target, float d, Func<float, float> e) =>
-        AddTween(new Tween<Vector2>(rt.sizeDelta, target, d, e, v => rt.sizeDelta = v, LerpVector2));
-
-    // Int tweening - generic for any int property (follows existing Tweening.To pattern)
-    public static Tween<int> TweenInt(int startValue, int endValue, float duration, Action<int> setter, EaseType easeType) =>
-        AddTween(new Tween<int>(startValue, endValue, duration, null, setter, LerpInt).SetEase(easeType));
-
-    public static Tween<int> TweenInt(int startValue, int endValue, float duration, Action<int> setter, Func<float, float> customEase) =>
-        AddTween(new Tween<int>(startValue, endValue, duration, customEase, setter, LerpInt));
-
-    // Rect tweening - generic for any Rect property (follows existing Tweening.To pattern)
-    public static Tween<Rect> TweenRect(Rect startValue, Rect endValue, float duration, Action<Rect> setter, EaseType easeType) =>
-        AddTween(new Tween<Rect>(startValue, endValue, duration, null, setter, LerpRect).SetEase(easeType));
-
-    public static Tween<Rect> TweenRect(Rect startValue, Rect endValue, float duration, Action<Rect> setter, Func<float, float> customEase) =>
-        AddTween(new Tween<Rect>(startValue, endValue, duration, customEase, setter, LerpRect));
 
     static Tween<T> AddTween<T>(Tween<T> tween)
     {
