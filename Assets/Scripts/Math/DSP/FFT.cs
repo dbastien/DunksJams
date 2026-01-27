c030a0b0266c8cb150a46c9aedeaebfda3b89ff2
dbastien <dwbastien@gmail.com>
2026-01-27 15:12:06 -0800

    Implement FFT internal transform

--- Assets/Scripts/Math/DSP/FFT.cs
+++ Assets/Scripts/Math/DSP/FFT.cs
@@ -3,10 +3,6 @@ using System.Numerics;
 
 public static class FFT
 {
-    /// <summary>
-    /// Compute the FFT of a real-valued signal
-    /// Returns complex frequency domain data
-    /// </summary>
     public static Complex[] Forward(float[] realInput)
     {
         int n = realInput.Length;
@@ -20,10 +16,7 @@ public static class FFT
 
         return Forward(complexInput);
     }
-
-    /// <summary>
-    /// Compute the FFT of complex input
-    /// </summary>
+    
     public static Complex[] Forward(Complex[] input)
     {
         int n = input.Length;
@@ -35,10 +28,7 @@ public static class FFT
 
         return output;
     }
-
-    /// <summary>
-    /// Compute the inverse FFT
-    /// </summary>
+    
     public static Complex[] Inverse(Complex[] input)
     {
         int n = input.Length;
@@ -60,10 +50,7 @@ public static class FFT
 
         return output;
     }
-
-    /// <summary>
-    /// Convert complex FFT output to magnitude spectrum
-    /// </summary>
+    
     public static float[] MagnitudeSpectrum(Complex[] fftData)
     {
         float[] magnitudes = new float[fftData.Length / 2];
@@ -75,10 +62,7 @@ public static class FFT
 
         return magnitudes;
     }
-
-    /// <summary>
-    /// Convert complex FFT output to magnitude spectrum in dB
-    /// </summary>
+    
     public static float[] MagnitudeSpectrumDB(Complex[] fftData, float reference = 1f)
     {
         float[] magnitudes = MagnitudeSpectrum(fftData);
@@ -99,9 +83,58 @@ public static class FFT
         return dbMagnitudes;
     }
 
-    // Internal FFT implementation using Cooley-Tukey algorithm
     private static void FFTInternal(Complex[] data, bool inverse)
     {
-        //TBD
+        int n = data.Length;
+        if (n <= 1) return;
+        if ((n & (n - 1)) != 0)
+        {
+            throw new ArgumentException("FFT length must be a power of two.", nameof(data));
+        }
+
+        for (int i = 1, j = 0; i < n; ++i)
+        {
+            int bit = n >> 1;
+            for (; (j & bit) != 0; bit >>= 1)
+            {
+                j ^= bit;
+            }
+            j ^= bit;
+            if (i < j)
+            {
+                (data[i], data[j]) = (data[j], data[i]);
+            }
+        }
+
+        float sign = inverse ? 1f : -1f;
+
+        for (int len = 2; len <= n; len <<= 1)
+        {
+            float angle = sign * 2f * MathF.PI / len;
+            Complex wLen = new Complex(MathF.Cos(angle), MathF.Sin(angle));
+            int halfLen = len >> 1;
+
+            for (int i = 0; i < n; i += len)
+            {
+                Complex w = Complex.One;
+                for (int j = 0; j < halfLen; ++j)
+                {
+                    Complex u = data[i + j];
+                    Complex v = w * data[i + j + halfLen];
+                    data[i + j] = u + v;
+                    data[i + j + halfLen] = u - v;
+                    w *= wLen;
+                }
+            }
+        }
+
+        if (inverse)
+        {
+            double invN = 1.0 / n;
+            for (int i = 0; i < n; ++i)
+            {
+                data[i] *= invN;
+            }
+        }
     }
 }
